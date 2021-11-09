1c102750622a3dca6284b1f79262011948cda534
karapho712 <newhijabmuallif@gmail.com>
2021-11-10 05:06:04 +0800

    FEAT: SIMPLE CRUD DONE

---
+++ app/Http/Controllers/ListController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\ListDaftar;
+
+class ListController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // return 'test view here';
+        $list = ListDaftar::get();
+
+        return view('list.index')->with('list', $list);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        // return 'test create here';
+
+        return view('list.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+        ListDaftar::create($data);
+
+        return redirect()->to('/list');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $data = ListDaftar::findOrFail($id);
+
+        return view('list.edit')->with('data', $data);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $data = $request->all();
+
+        $idlist = ListDaftar::findOrFail($id);
+
+        $idlist->update($data);
+
+        return redirect()->to('/list');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $item = ListDaftar::findOrFail($id);
+
+        $item->delete();
+
+        return redirect()->to('/list');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ListController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/list', [ListController::class, 'index']);
+Route::get('/list-create', [ListController::class, 'create']);
+Route::post('/list-store', [ListController::class, 'store']);
+Route::get('/list-edit/{id}', [ListController::class, 'edit']);
+Route::put('/list-update/{id}', [ListController::class, 'update']);
+Route::delete('/list-delete/{id}', [ListController::class, 'destroy']);
